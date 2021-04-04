5cc9595f78a3e7296de7093956d5d1b3efcc5fd9
Gleison Batista Soares <gleisonbs@gmail.com>
2021-04-04 14:20:46 -0300

    feat: add Reactivities API

---
+++ API/Controllers/Activities.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+  public class ActivitiesController : BaseApiController
+  {
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetAtivities()
+    {
+      return await Mediator.Send(new List.Query());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+      return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity(Activity activity)
+    {
+      activity.Id = Guid.NewGuid();
+      await Mediator.Send(new Create.Command { Activity = activity });
+      return Ok(new { id = activity.Id });
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+      activity.Id = id;
+      return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+      return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+    }
+  }
+}
