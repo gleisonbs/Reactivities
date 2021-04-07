85198cb1c86d7b5ec50c05eb95784cc494c3916d
Gleison Batista Soares <gleisonbs@gmail.com>
2021-04-07 20:15:53 -0300

    feat: make navbar items navlinks

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
 
 export default function NavBar() {
-  const { activityStore } = useStore();
-
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item exact as={NavLink} to="/" header>
           <img
             src="/assets/logo.png"
             alt="logo"
@@ -18,10 +16,11 @@ export default function NavBar() {
           />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" />
+        <Menu.Item exact as={NavLink} to="/activities" name="Activities" />
         <Menu.Item>
           <Button
-            onClick={() => activityStore.openForm()}
+            as={NavLink}
+            to="/activities/new"
             positive
             content="Create Activity"
           />
