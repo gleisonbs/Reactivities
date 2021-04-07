25c0dc9a4f42cefbdfbdba0a772c79da7a2aef4e
Gleison Batista Soares <gleisonbs@gmail.com>
2021-04-07 20:15:29 -0300

    feat: add routes to app

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,32 +1,47 @@
-import { useEffect } from "react";
+import React from "react";
 import { observer } from "mobx-react-lite";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadComponent";
-import { useStore } from "../stores/store";
+import { Route, Switch } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) {
-    return <LoadingComponent content="Loading activities" />;
-  }
-
+  const location = useLocation();
   return (
     <>
-      <NavBar />
-      <Container
-        style={{
-          marginTop: "7em",
-        }}
-      >
-        <ActivityDashboard />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar />
+            <Container
+              style={{
+                marginTop: "7em",
+              }}
+            >
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route
+                  key={location.key}
+                  exact
+                  path={["/activities/new", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route
+                  exact
+                  path="/activities/:id"
+                  component={ActivityDetails}
+                />
+              </Switch>
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
